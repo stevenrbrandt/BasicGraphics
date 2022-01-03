f794bb4d8055c4aa572de361a4d8cc7962103abc
Steven R. Brandt <sbrandt@cct.lsu.edu>
2022-01-03 14:46:57 -0600

    Initial commit of BasicGraphics
    Make the settings more portable
    
    Fix deprecation
    
    Add build/classes
    
    Fix sound system
    
    Fix project setup
    
    Remove lunar lander
    
    Add sounds
    
    Get sound working
    
    Better verbose
    
    Translate %20 to space
    
    Add Lights Out game.
    
    Remove full calculator

---
+++ src/basicgraphics/sounds/ReusableClip.java
@@ -0,0 +1,137 @@
+package basicgraphics.sounds;
+
+import basicgraphics.BasicFrame;
+import basicgraphics.FileUtility;
+import java.io.File;
+import java.util.LinkedList;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
+
+/**
+ * An interface to the java sound system
+ *
+ * @author Steven R. Brandt
+ */
+public final class ReusableClip {
+
+    byte[] buf;
+    SourceDataLine sourceLine;
+    
+    static Thread thread;
+    static boolean running = false;
+    static LinkedList<ReusableClip> queue = new LinkedList<>();
+    static boolean verbose = false;
+    
+    static {
+        thread = new Thread(()->{});
+        thread.start();
+    }
+    
+    private static synchronized ReusableClip dequeue() {
+        if(queue.size() == 0) return null;
+        else return queue.removeLast();
+    }
+    
+    private static synchronized void done() {
+        running = false;
+    }
+    
+    private static synchronized void enqueue(ReusableClip r) {
+        queue.addFirst(r);
+        if(!running) {
+            try {
+                thread.join();
+            } catch (InterruptedException ex) {
+            }
+            running = true;
+            thread = new Thread(()->{
+                while(true) {
+                    ReusableClip clip = dequeue();
+                    if(clip == null)
+                        break;
+                    try {
+                        clip.playNow();
+                    if(clip.looping)
+                        enqueue(clip);
+                    } catch(Throwable e) {
+                        System.err.println("endclip: "+e);
+                        if(verbose)
+                            e.printStackTrace();
+                    }
+                }
+                done();
+            });
+            thread.start();
+        }
+    }
+    public ReusableClip(String name) {
+        this(FileUtility.findFile(ReusableClip.class, name));
+    }
+    final String name;
+    public ReusableClip(File f) {
+        this.name = f.getName();
+        try {
+            if(verbose)
+                System.out.println("loading sound: "+f);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(f);
+            AudioFormat audioFormat = audioStream.getFormat();
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+            sourceLine = (SourceDataLine) AudioSystem.getLine(info);
+            sourceLine.open(audioFormat);
+            long nbytes = audioStream.getFrameLength();
+            while(nbytes % 4 != 0) nbytes++;
+            buf = new byte[(int)nbytes];
+            audioStream.read(buf);
+        } catch (Exception ex) {
+            System.err.println(ex);
+            if(verbose)
+                ex.printStackTrace();
+        }
+    }
+    
+    public void playNow() {
+        if(verbose)
+            System.out.println("playing sound: "+name);
+        sourceLine.start();
+        sourceLine.write(buf, 0, buf.length);
+        sourceLine.drain();
+        sourceLine.flush();
+    }
+
+    public synchronized void play() {
+        looping = false;
+        enqueue(this);
+    }
+
+    volatile boolean looping = false;
+    public synchronized void loop() {
+        looping = true;
+        enqueue(this);
+    }
+
+    public void stop() {
+        looping = false;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // It won't play without a JFrame.
+        verbose = true;
+        BasicFrame bf = new BasicFrame("title");
+        bf.show();
+        ReusableClip clip1 = new ReusableClip("arrow.wav");
+        ReusableClip clip2 = new ReusableClip("beep.wav");
+        long t1 = System.currentTimeMillis();
+        clip1.loop();
+        clip2.loop();
+        Thread.sleep(15000);
+        clip1.stop();
+        clip2.stop();
+        long t2 = System.currentTimeMillis();
+        System.out.printf("time=%d%n",(t2-t1));
+        bf.dispose();
+    }
+}
---
+++ src/lightsout/LightsOutGame.java
@@ -0,0 +1,66 @@
+package lightsout;
+
+import basicgraphics.BasicFrame;
+import basicgraphics.Clock;
+import basicgraphics.SpriteComponent;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class LightsOutGame {
+    public static final int TILE_SIZE = 75;
+    public static final int N_TILES = 3;
+    public static final int PROBLEM_STEPS = 10;
+    public static final Dimension WINDOW_SIZE = new Dimension(TILE_SIZE * N_TILES,TILE_SIZE * N_TILES);
+
+    private SpriteComponent sc;
+    private final BasicFrame frame = new BasicFrame("Lights Out");
+
+    Tile[][] tiles;
+    int litTiles = 0;
+
+    public void run() {
+        sc = new SpriteComponent();
+        sc.setPreferredSize(WINDOW_SIZE);
+        String[][] layout = {{"center"}};
+        frame.setStringLayout(layout);
+        frame.add("center", sc);
+
+        tiles = new Tile[N_TILES][N_TILES];
+        for (int i = 0; i < N_TILES; i++) {
+            tiles[i] = new Tile[N_TILES];
+            for (int j = 0; j < N_TILES; j++) {
+                tiles[i][j] = new Tile(sc, this, j, i, false);
+            }
+        }
+
+        generateProblem();
+
+        frame.show();
+        Clock.start(1);
+        Clock.addTask(sc.moveSprites());
+    }
+
+    // Generating the board with a sequence of toggleWithNeighbors guarantees the board is solvable
+    public void generateProblem() {
+        var rand = new Random();
+        for (int i = 0; i < PROBLEM_STEPS; i++) {
+            var tile = tiles[rand.nextInt(N_TILES)][rand.nextInt(N_TILES)];
+            tile.toggleWithNeighbors(false);
+        }
+    }
+
+    public void checkWin() {
+        if (litTiles == 0) {
+            JOptionPane.showMessageDialog(sc, "You're winner!");
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        var game = new LightsOutGame();
+        game.run();
+    }
+}
